05e6327364d13bfbbe2d12f4faa058070251fe2b
DimS <dimsed13@yandex.ru>
2022-02-27 18:10:29 +0300

    Задача 7 решена

---
+++ task2_3/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Программа выводит третью цифру числа, либо сообщает, что её нет.
+Console.WriteLine("Введите число ");
+string val1 = Console.ReadLine();
+val1 = val1.Trim();
+
+if (val1.Length < 3) 
+{
+    Console.WriteLine("Третьей цифры в этом числе нет");
+    return;
+}
+
+char res = val1[2];
+
+Console.WriteLine("Третья цифра числа " + res);
