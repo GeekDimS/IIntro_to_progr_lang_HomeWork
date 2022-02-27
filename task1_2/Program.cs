10f0954e6e0f76e3295ea8f26647ccf0fbb95e1d
DimS <dimsed13@yandex.ru>
2022-02-27 16:10:37 +0300

    Задача 2 решена

---
+++ task1_2/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите 3 числа через Enter");
+int val1 = int.Parse(Console.ReadLine());
+int val2 = int.Parse(Console.ReadLine());
+int val3 = int.Parse(Console.ReadLine());
+
+int max = val1;
+if (max < val2) max = val2;
+if (max < val3) max = val3;
+
+Console.WriteLine("Максимальное из введёных чисел " + max);
