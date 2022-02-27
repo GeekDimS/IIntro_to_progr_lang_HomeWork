99c700cd7ed4ed2d928e836bf673a1d7a1151189
DimS <dimsed13@yandex.ru>
2022-02-27 17:20:53 +0300

    Задача 5 решена

---
+++ task2_1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Программа принимает на вход 3-хзначное число и выдаёт вторую цифру этого числа
+Console.WriteLine("Введите трёхзначное число");
+int val1 = int.Parse(Console.ReadLine());
+
+int rem = val1 % 10;
+val1 = val1 - rem;
+rem = val1 % 100;
+val1 = rem / 10;
+
+Console.WriteLine("Вторая цифра числа " + val1);
