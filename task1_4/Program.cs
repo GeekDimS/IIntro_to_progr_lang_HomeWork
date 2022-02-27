d50994d9563aa15f9c14e702ba146f6ce93e034a
DimS <dimsed13@yandex.ru>
2022-02-27 16:47:48 +0300

    Задача 4 решена

---
+++ task1_4/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Программа принимает на вход число и выдаёт все чётные числа от 1
+// до этого числа
+Console.WriteLine("Введите число");
+int val1 = int.Parse(Console.ReadLine());
+
+for (int i = 2; i <= val1; i = i + 2 )
+{
+    Console.Write(i + " ");
+}
+
+Console.WriteLine();
