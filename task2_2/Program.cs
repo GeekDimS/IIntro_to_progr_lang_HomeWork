3f4e9cb85c6a81b02da1ea7a56bbe6ff29150e8a
DimS <dimsed13@yandex.ru>
2022-02-27 17:44:59 +0300

    Задача 6 решена

---
+++ task2_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Программа выводит случайное 3-хначное число и удаляет вторую цифру
+// этого числа
+Random rnd = new Random();
+int val = rnd.Next(100, 1000);
+
+Console.WriteLine("Случайное число " + val);
+
+int rem = val % 10;
+int div = val / 100;
+val = (div*10) + rem;
+
+Console.WriteLine("Число без средней цифры " + val);
