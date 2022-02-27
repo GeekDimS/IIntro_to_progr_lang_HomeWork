42132c8a45c9342a03e27a59d4c3278e80e0c6e1
DimS <dimsed13@yandex.ru>
2022-02-27 16:19:14 +0300

    Задача 3 решена

---
+++ task1_3/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Программа принимает на вход число и выдаёт, чётное оно или нет.
+Console.WriteLine("Введите число");
+int val1 = int.Parse(Console.ReadLine());
+
+int res = val1 % 2;
+if ((val1 % 2) == 0) Console.WriteLine("Число чётное");
+else Console.WriteLine("Число нечётное");
