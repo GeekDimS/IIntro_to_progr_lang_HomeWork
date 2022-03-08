52e0ba048096921d9cd13a9ff290b7ca3379955c
DimS <dimsed13@yandex.ru>
2022-03-09 01:41:16 +0300

    Решение 1 задачи с двумя звёздами 3 семинара

---
+++ task3_1_star_star/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine(int.MaxValue + "   " + int.MinValue);
+// Программа определения числа разрядности до Integer на палиндромность
+string PromtStr(string message) //Функция ввода символов как строки символов
+{
+    Console.WriteLine(message);
+    return Console.ReadLine();
+}
+
+bool Palindrom(string message) //Функция определения, является-ли строка палиндромом
+{
+    int ls = message.Length;
+    int ls2 = ls/2;
+    string a = message.Substring(0, ls2);
+    string b = "";
+    for(int i = ls-1; i >= (ls-ls2); i--)
+    {
+        b = b + message[i];
+    }
+    if (a == b) return true;
+    else return false;
+}
+
+string valS = PromtStr("Введите число не более " + int.MaxValue);
+valS = valS.Trim();
+int valI = 0;
+if (!Int32.TryParse(valS, out valI)) //Проверка на наличие только цифр на вводе
+{
+    Console.WriteLine("Число введено в неправильном формате");
+    return;
+}
+
+if (Palindrom(valS)) Console.WriteLine("Число "+ valS + " является палиндромом");
+else Console.WriteLine("Число "+ valS + " не является палиндромом");
