bc577672fb1954d61eb74cfbb27194a6bfee2b83
DimS <dimsed13@yandex.ru>
2022-03-08 20:04:17 +0300

    Решена 3 задача 3-го семинара

---
+++ task3_3/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//Программа принимает на вход целое число N и выводит кубы всех чисел от 1 до N
+int PromtInt(string message) //Функция ввода символов как целого числа
+{
+    Console.WriteLine(message);
+    return int.Parse(Console.ReadLine());
+}
+int N = PromtInt("Введите целое число");
+for (int i = 1; i <= N; i++)
+{
+    Console.Write((i*i*i) + ", ");
+}
