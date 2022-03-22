db7fc1cc0607c49f59b5a6b6ff5414b5927c045a
DimS <dimsed13@yandex.ru>
2022-03-23 01:19:02 +0300

    Создана библиотека функций и решена задача 4_1

---
+++ Methods/Program.cs
@@ -0,0 +1,62 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+// Здесь собираются методы (подпрограммы, функции) для сокращения написания
+// новых программ
+
+int PromtInt(string message) //Функция ввода символов как целого числа. Запрашивает на входе
+// строку запроса, какие именно данные нужны
+{
+    Console.WriteLine(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int PromtInt(string message) //Функция ввода символов как целого числа с проверкой. При вызове нужна
+// строка запроса, какие именно данные нужны. 
+{
+    Console.WriteLine(message);
+    int temp = 0;
+    if (int.TryParse(Console.ReadLine(), out temp))
+        return temp;
+    else
+    {
+        Console.WriteLine("Неверно введено число");
+        return -1;
+    }
+}
+
+string PromtStr(string message) //Функция ввода символов как строки символов. Запрашивает на входе
+// строку запроса, какие именно данные нужны
+{
+    Console.WriteLine(message);
+    return Console.ReadLine();
+}
+
+double PromtDouble(string message) //Функция ввода символов как числа с дробной частью с проверкой.
+// При вызове нужна строка запроса, какие именно данные нужны
+{
+    Console.WriteLine(message);
+    double temp = 0;
+    if (double.TryParse(Console.ReadLine(), out temp))
+        return temp;
+    else
+    {
+        Console.WriteLine("Неверно введено число");
+        return -1;
+    }
+}
+
+bool Palindrom(string message) //Функция определения, является-ли строка палиндромом
+{
+    int ls = message.Length;
+    int ls2 = ls / 2;
+    string a = message.Substring(0, ls2);
+    string b = "";
+    for (int i = ls - 1; i >= (ls - ls2); i--)
+    {
+        b = b + message[i];
+    }
+    if (a == b)
+        return true;
+    else
+        return false;
+}
---
+++ task4_1/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+// Цикл, к-рый принимает на вх. 2 числа А и В и возводит А в натуральную степень В
+
+int PromtInt(string message) //Функция ввода символов как целого числа с проверкой. При вызове нужна
+// строка запроса, какие именно данные нужны. 
+{
+    Console.WriteLine(message);
+    int temp = 0;
+    if (int.TryParse(Console.ReadLine(), out temp))
+        return temp;
+    else
+    {
+        Console.WriteLine("Неверно введено число");
+        return -1;
+    }
+}
+
+double PromtDouble(string message) //Функция ввода символов как числа с дробной частью с проверкой.
+// При вызове нужна строка запроса, какие именно данные нужны
+{
+    Console.WriteLine(message);
+    double temp = 0;
+    if (double.TryParse(Console.ReadLine(), out temp))
+        return temp;
+    else
+    {
+        Console.WriteLine("Неверно введено число");
+        return -1;
+    }
+}
+
+double numbA = PromtDouble("Введите число, которое надо возвести в степень (основание) ");
+if (numbA == -1)
+    return;
+int numbB = PromtInt("Теперь введите натуральную степень, в которую надо возвести основание ");
+if (numbB == -1 || numbB < 0)
+{
+    Console.WriteLine("Ошибка: степень отрицательная");
+    return;
+}
+double res = 1;
+
+for (int i = 1; i <= numbB; i++)
+{
+    res = res * numbA;
+}
+
+Console.WriteLine("Ответ: " + res);
