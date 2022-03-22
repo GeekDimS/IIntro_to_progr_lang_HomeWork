35f45bfc6909b4d5509061fbed9e78e0eb5ac89b
DimS <dimsed13@yandex.ru>
2022-03-23 01:52:00 +0300

    Решена задача 4_2

---
+++ task4_2/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+// Программа принимает на вход число и выдаёт сумму цифр в числе. Предположу, что 
+// входное число должно быть целым.
+
+int PromtInt(string message) //Функция ввода символов как целого числа с проверкой. При вызове нужна
+// строка запроса, какие именно данные нужны. 
+{
+    Console.WriteLine(message);
+    int temp = 0;
+    if (int.TryParse(Console.ReadLine(), out temp))
+        return temp;
+    else
+    {
+        Console.WriteLine("Неверно введено число");
+        return -1;
+    }
+}
+
+int inpVal = PromtInt("Введите натуральное число ");
+if(inpVal == -1) return; // Выход из программы, если ввод ошибочный
+
+int res = 0;
+while(inpVal != 0)
+{
+    res = res + inpVal % 10;
+    inpVal = inpVal / 10;
+}
+
+if(res<0) res = -1*res; // Если введено отрицательное число, то убираем лишний минус в сумме
+Console.WriteLine("Сумма цифр в числе = " + res);
