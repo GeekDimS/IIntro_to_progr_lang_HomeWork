6731997ce1356a008d6e072a6323e9a2e59b83e2
DimS <dimsed13@yandex.ru>
2022-04-16 17:38:14 +0300

    Решена задача 1 из д/з 11 семинара

---
+++ task11_1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Определите, является ли число степенью двойки:
+// N = 16 -> "Является степень двойки"
+// N = 12 -> “Не является степенью двойки”
+
+int PromtIntCheck(string message) //Функция ввода символов как целого числа с проверкой. Можно указать
+// строка запроса, какие именно данные нужны.
+{
+    Console.WriteLine(message);
+    if (int.TryParse(Console.ReadLine(), out int temp))
+        return temp;
+    else
+    {
+        Console.WriteLine("Неверно введено число");
+        return 0;
+    }
+}
+
+bool CheckTwoPower(int N = 0)
+{
+    if (N == 1)
+        return true;
+    if (N <= 0)
+        return false;
+    if (N % 2 != 0)
+        return false;
+    return CheckTwoPower(N / 2);
+}
+
+int N = PromtIntCheck("Введите число для проверки: ");
+if (CheckTwoPower(N))
+    Console.WriteLine("Является степень двойки");
+else
+    Console.WriteLine("Не является степенью двойки");
